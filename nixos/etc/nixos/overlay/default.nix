1bde06487510d0e8159f42bb984fc3f7dde7782f
Mark Cohen <git@mail.mpc.sh>
2017-09-27 09:14:03 -0500

    Remove no-longer-necessary modules

--- nixos/etc/nixos/overlay/default.nix
+++
@@ -1,7 +0,0 @@
-self: super:
-{
-  ## example:
-  # shblah-git = super.callPackage ./shblah { };
-  polybar-git = super.callPackage ./polybar-git { };
-  z-git = super.callPackage ./z-git { };
-}
--- nixos/etc/nixos/profiles/alpamayo/boot.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  hardware.cpu.intel.updateMicrocode = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  boot.loader.systemd-boot.enable = true;
-}
