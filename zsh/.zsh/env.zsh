666e8df57b21cc65a9ee568b17a98064f9cfccb3
Mark Cohen <git@mail.mpc.sh>
2017-10-02 09:31:38 -0500

    Let iTerm2 take care of setting TERM

--- zsh/.zsh/env.zsh
+++ zsh/.zsh/env.zsh
@@ -2,7 +2,7 @@
 export GOPATH=~/.go
 export EDITOR=nvim
 export PAGER="less -FRX" # don't use pager for `git ls`, etc
-export TERM=xterm-256color # for ssh compatibility
+# export TERM=xterm-256color # for ssh compatibility
 export TZ=:/etc/localtime # https://blog.packagecloud.io/eng/2017/02/21/set-environment-variable-save-thousands-of-system-calls/
 
 # zsh configuration
