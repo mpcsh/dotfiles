37606bb24efd8fb162d7eb255f8a8a5dd3605e42
Mark Cohen <git@mail.mpc.sh>
2017-10-19 11:43:37 -0500

    Moving to fish!

--- zsh/.zsh/history.zsh
+++
@@ -1,8 +0,0 @@
-# get shared history all working properly (thanks @oconnor663)
-HISTSIZE=100000
-SAVEHIST=$HISTSIZE
-setopt extended_history
-setopt hist_ignore_dups
-setopt hist_ignore_space
-setopt inc_append_history
-setopt share_history
