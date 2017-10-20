5bc784f5f63169012430c48b9632271a21549216
Mark Cohen <git@mail.mpc.sh>
2017-10-19 22:54:31 -0500

    Fishified aliases

---
+++ fish/.config/fish/conf.d/aliases.fish
@@ -0,0 +1,77 @@
+# allows sudo to use aliases
+function sudo
+	command sudo $argv
+end
+
+
+# better ls
+function ls
+	command ls -lhFG $argv
+end
+
+function la
+	command ls -lhFGA $argv
+end
+
+
+# symlink by default
+function ln
+	command ln -s $argv
+end
+
+
+# work on directories
+function cp
+	command cp -r $argv
+end
+
+function rm
+	command rm -r $argv
+end
+
+function mkdir
+	command mkdir -p $argv
+end
+
+
+# shred and delete
+function shred
+	command shred --remove $argv
+end
+
+
+# typed racket
+function racket
+	command racket -I typed/racket $argv
+end
+
+
+# ack
+function ack
+	command ack --smart-case --color-match=green $argv
+end
+
+
+# nvim
+function vim
+	command nvim $argv
+end
+
+
+# rsync options
+function rsync
+	command rsync -avzHE $argv
+end
+
+
+# mkdir and cd into it
+function mkcd
+	command mkdir -p $argv
+	command cd $argv
+end
+
+
+# cd .. repeatedly
+function up
+	cd (eval printf ../%.0s (seq $argv))
+end
