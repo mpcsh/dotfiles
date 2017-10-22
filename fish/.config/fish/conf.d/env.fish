f8660e4d6ecc3dddada89e481bf0ba76da601797
Mark Cohen <git@mail.mpc.sh>
2017-10-21 23:54:42 -0500

    Make envvars global scope instead of universal; make pwd always show full path

--- fish/.config/fish/conf.d/env.fish
+++ fish/.config/fish/conf.d/env.fish
@@ -1,5 +1,6 @@
 # environment variables
-set -Ux GOPATH ~/.go
-set -Ux EDITOR nvim
-set -Ux FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -l -g ""'
-set -Ux FZF_DEFAULT_OPTS '--color=16,fg+:4'
+set -gx GOPATH ~/.go
+set -gx EDITOR nvim
+set -gx FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -l -g ""'
+set -gx FZF_DEFAULT_OPTS '--color=16,fg+:4'
+set -gx fish_prompt_pwd_dir_length 0
