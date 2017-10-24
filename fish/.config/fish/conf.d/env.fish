ba593d70591acf7aba48ae923031923788bf5b20
Mark Cohen <git@mail.mpc.sh>
2017-10-23 20:33:34 -0500

    Add GOPATH/bin to PATH

--- fish/.config/fish/conf.d/env.fish
+++ fish/.config/fish/conf.d/env.fish
@@ -1,5 +1,6 @@
 # environment variables
 set -gx GOPATH ~/.go
+set -gx fish_user_paths $GOPATH/bin
 set -gx EDITOR nvim
 set -gx FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -l -g ""'
 set -gx FZF_DEFAULT_OPTS '--color=16,fg+:4'
