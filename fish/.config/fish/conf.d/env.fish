7b6675197786c4537b1c0392e03c9a9c1aad52d9
Mark Cohen <git@mail.mpc.sh>
2017-10-23 20:15:17 -0500

    Use fzf C-t!

--- fish/.config/fish/conf.d/env.fish
+++ fish/.config/fish/conf.d/env.fish
@@ -3,4 +3,5 @@ set -gx GOPATH ~/.go
 set -gx EDITOR nvim
 set -gx FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -l -g ""'
 set -gx FZF_DEFAULT_OPTS '--color=16,fg+:4'
+set -gx FZF_CTRL_T_COMMAND 'ag --hidden --ignore .git -l -g ""'
 set -gx fish_prompt_pwd_dir_length 0
