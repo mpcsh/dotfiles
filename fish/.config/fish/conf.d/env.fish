06e3e8ada9d90a7488a255e476e092843de88f16
Mark Cohen <git@mail.mpc.sh>
2017-10-24 21:32:36 -0500

    Reverse C-r with fzf

--- fish/.config/fish/conf.d/env.fish
+++ fish/.config/fish/conf.d/env.fish
@@ -5,4 +5,5 @@ set -gx EDITOR nvim
 set -gx FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -l -g ""'
 set -gx FZF_DEFAULT_OPTS '--color=16,fg+:4'
 set -gx FZF_CTRL_T_COMMAND 'ag --hidden --ignore .git -l -g ""'
+set -gx FZF_CTRL_R_OPTS '--reverse'
 set -gx fish_prompt_pwd_dir_length 0
