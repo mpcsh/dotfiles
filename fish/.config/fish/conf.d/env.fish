d17553613aaa95688f81f0e37a17f2ff637dc4ea
Mark Cohen <git@mail.mpc.sh>
2017-10-27 11:45:14 -0500

    Brew command not found was making shit super slow

--- fish/.config/fish/conf.d/env.fish
+++ fish/.config/fish/conf.d/env.fish
@@ -7,6 +7,3 @@ set -gx FZF_DEFAULT_OPTS '--color=16,fg+:4'
 set -gx FZF_CTRL_T_COMMAND 'ag --hidden --ignore .git -l -g ""'
 set -gx FZF_CTRL_R_OPTS '--reverse'
 set -gx fish_prompt_pwd_dir_length 0
-
-# homebrew command not found
-brew command command-not-found-init > /dev/null 2>&1; and . (brew command-not-found-init)
