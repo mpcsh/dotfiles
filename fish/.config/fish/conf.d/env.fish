cf2a80cf0252f9b0e0663140339f92ec447e626e
Mark Cohen <git@mail.mpc.sh>
2017-11-10 15:49:56 -0600

    Make fzf use --ansi by default (for colored input & whatnot)

--- fish/.config/fish/conf.d/env.fish
+++ fish/.config/fish/conf.d/env.fish
@@ -3,7 +3,7 @@ set -gx GOPATH ~/.go
 set -gx fish_user_paths $GOPATH/bin
 set -gx EDITOR nvim
 set -gx FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -l -g ""'
-set -gx FZF_DEFAULT_OPTS '--color=16,fg+:4'
+set -gx FZF_DEFAULT_OPTS '--color=16,fg+:4 --ansi'
 set -gx FZF_CTRL_T_COMMAND 'ag --hidden --ignore .git -l -g ""'
 set -gx FZF_CTRL_R_OPTS '--reverse'
 set -gx fish_prompt_pwd_dir_length 0
