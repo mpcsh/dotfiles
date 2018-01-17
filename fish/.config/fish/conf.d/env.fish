b0ead40aaa71a7af7def1f0f10da92cfc22af135
Mark Cohen <git@mail.mpc.sh>
2018-01-17 16:10:59 -0600

    Pyenv

--- fish/.config/fish/conf.d/env.fish
+++ fish/.config/fish/conf.d/env.fish
@@ -3,7 +3,8 @@ set fish_greeting ""
 
 # environment variables
 set -gx GOPATH ~/.go
-set -gx fish_user_paths $GOPATH/bin /usr/local/sbin
+set -gx PYENV_ROOT ~/.pyenv
+set -gx fish_user_paths $GOPATH/bin /usr/local/sbin $PYENV_ROOT/shims $PYENV_ROOT/bin
 set -gx EDITOR nvim
 set -gx FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -l -g ""'
 set -gx FZF_DEFAULT_OPTS '--color=16,fg+:4 --ansi'
@@ -13,3 +14,7 @@ set -gx fish_prompt_pwd_dir_length 0
 
 # permissions
 umask 077
+
+# pyenv
+status --is-interactive; and . (pyenv init -|psub)
+status --is-interactive; and . (pyenv virtualenv-init -|psub)
