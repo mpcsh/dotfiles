eccad7561e9d615b57550a7163ff403d8ffd0c1c
Mark Cohen <git@mail.mpc.sh>
2018-04-03 17:35:21 -0500

    Minimal libinput config

--- nixos/etc/nixos/profiles/quitaraju/hostname.nix
+++ nixos/etc/nixos/profiles/quitaraju/hostname.nix
@@ -7,28 +7,28 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.enable = true;
 
-  # services.xserver.libinput = {
-  #   enable = true;
+  services.xserver.libinput = {
+    enable = true;
 
-  #   accelProfile = "flat";
+    accelProfile = "flat";
   #   buttonMapping = "1 3 2";
   #   clickMethod = "clickfinger";
   #   disableWhileTyping = true;
   #   middleEmulation = false;
   #   tappingDragLock = false;
   #   tapping = false;
-  # };
-  services.xserver.synaptics = rec {
-    enable = true;
-    accelFactor = "0";
-    additionalOptions = ''Option "CoastingSpeed" "0"'';
-    buttonsMap = [ 1 3 2 ];
-    horizEdgeScroll = false;
-    maxSpeed = "0.8";
-    minSpeed = maxSpeed;
-    palmDetect = true;
-    tapButtons = false;
-    twoFingerScroll = true;
-    vertEdgeScroll = false;
   };
+  # services.xserver.synaptics = rec {
+  #   enable = true;
+  #   accelFactor = "0";
+  #   additionalOptions = ''Option "CoastingSpeed" "0"'';
+  #   buttonsMap = [ 1 3 2 ];
+  #   horizEdgeScroll = false;
+  #   maxSpeed = "0.8";
+  #   minSpeed = maxSpeed;
+  #   palmDetect = true;
+  #   tapButtons = false;
+  #   twoFingerScroll = true;
+  #   vertEdgeScroll = false;
+  # };
 }
