5f82ae05e07bfb9f8f1950002795db0662eda069
Mark Cohen <git@mail.mpc.sh>
2018-04-04 21:26:21 -0500

    Move all illimani config into hostname.nix

--- nixos/etc/nixos/profiles/illimani/hostname.nix
+++ nixos/etc/nixos/profiles/illimani/hostname.nix
@@ -1,10 +1,27 @@
 { config, pkgs, ...}:
 
 {
-  imports = [
-    ./boot.nix
-    ./nvidia.nix
-  ];
-
   networking.hostName = "illimani";
+
+  hardware.cpu.intel.updateMicrocode = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.loader.grub = {
+    # required for uefi
+    device = "nodev";
+    efiSupport = true;
+
+    # winblows entry
+    extraEntries = ''
+      menuentry "Winblows 8.1" {
+      insmod part_gpt
+      insmod search_fs_uuid
+      insmod chain
+      search --fs-uuid --no-floppy --set=root 4023-cd4e
+      chainloader ($root)/EFI/MICROSOFT/BOOT/bootmgfw.efi
+      }
+    '';
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
 }
