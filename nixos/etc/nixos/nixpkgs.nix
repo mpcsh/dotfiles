fe8b2ce6bd61d9125a9b98def8c82d950e9f2c04
Mark Cohen <git@mail.mpc.sh>
2018-04-11 22:25:53 -0500

    Remove automatic nixos-rebuild switch and nix-collect-garbage services. Could they be the cause of the firefox window-switching issue?

--- nixos/etc/nixos/nixpkgs.nix
+++ nixos/etc/nixos/nixpkgs.nix
@@ -7,17 +7,5 @@
 
   nix.useSandbox = true;
 
-  system.autoUpgrade = {
-    enable = true;
-    channel = https://nixos.org/channels/nixos-unstable;
-    dates = "0/1:00:00"; # means "every hour on the hour"
-  };
-
-  # run nix-collect-garbage automatically
-  nix.gc = {
-    automatic = true;
-    dates = "0/1:00:00"; # means "every hour on the hour"
-  };
-
   system.stateVersion = "unstable";
 }
