ffe04d5510e6d298c31ae8f31907f0b331aec8f5
Mark Cohen <git@mail.mpc.sh>
2018-04-29 02:38:03 -0500

    Revert "Move back to xterm-256color"
    This reverts commit 234450c8ee516714b49f2099a11a8a03c8387a4f.

--- zsh/.zsh/env.zsh
+++ zsh/.zsh/env.zsh
@@ -1,16 +1,16 @@
 # environment variables
-export DISABLE_AUTO_TITLE="true" # https://github.com/robbyrussell/oh-my-zsh/issues/1338
 export EDITOR="nvim"
-export FZF_CTRL_R_OPTS="--reverse"
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_DEFAULT_COMMAND="command rg --hidden --smart-case --glob !.git --files-with-matches ."
 export FZF_DEFAULT_OPTS="--color=16,fg+:4 --ansi"
-export GIT_ASKPASS=""
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_CTRL_R_OPTS="--reverse"
 export GOPATH="$HOME/.go"
 export PAGER="less -FR"
 export PYENV_ROOT="~/.pyenv"
+export TERM="screen-256color"
+export DISABLE_AUTO_TITLE="true" # https://github.com/robbyrussell/oh-my-zsh/issues/1338
 export SSH_ASKPASS="" # https://gist.github.com/joseluisq/d0e66c5f718250c90075
-export TERM="xterm-256color"
+export GIT_ASKPASS=""
 
 # zsh configuration
 ENABLE_CORRECTION="true" # enable command autocorrection
