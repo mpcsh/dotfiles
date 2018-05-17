49347cb6b4badc9f0344f23ce7475610b12189c0
Mark Cohen <git@mail.mpc.sh>
2018-05-17 15:05:35 -0500

    Revert "Revert "Move back to fish!""
    This reverts commit e3bdefb877729c17c64929b917401fa4a403c8f5.

---
+++ fish/.config/fish/conf.d/aliases.fish
@@ -0,0 +1,83 @@
+# thefuck
+if type -q thefuck
+  thefuck --alias | source
+end
+
+# allows sudo to use aliases
+function sudo
+  command sudo -sE $argv
+end
+
+# better ls
+function ls
+  switch (uname)
+  case Darwin
+    command ls -lhFG $argv
+  case Linux
+    command ls -lhF --color=auto $argv
+  end
+end
+
+function la
+  ls -A $argv
+end
+
+# because I like pain
+function yay
+  command yay --noconfirm $argv
+end
+
+
+# symlink by default
+function ln
+  command ln -s $argv
+end
+
+
+# work on directories
+function cp
+  command cp -r $argv
+end
+
+function rm
+  command rm -rf $argv
+end
+
+function mkdir
+  command mkdir -p $argv
+end
+
+
+# typed racket
+function racket
+  command racket -I typed/racket $argv
+end
+
+# nvim
+function vim
+  command nvim $argv
+end
+
+
+# rsync options
+function rcp
+  command rsync -avzHE --progress -h $argv
+end
+
+
+# mkdir and cd into it
+function mkcd
+  command mkdir -p $argv
+  cd $argv
+end
+
+
+# cd .. repeatedly
+function up
+  cd (eval printf ../%.0s (seq 1 $argv))
+end
+
+# make CLI rg match fzf
+function rg
+  command rg --hidden --smart-case --glob !.git $argv
+end
--- nvim/.config/nvim/init.vim
+++ nvim/.config/nvim/init.vim
@@ -16,6 +16,7 @@ Plug 'wincent/terminus'
 Plug 'junegunn/goyo.vim' " distraction-free mode
 
 " language support
+Plug 'dag/vim-fish'
 Plug 'baskerville/vim-sxhkdrc'
 Plug 'lervag/vimtex'
 
