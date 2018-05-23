ad7373d749434ee3923203b2cb153b664b8ce089
Mark Cohen <git@mail.mpc.sh>
2018-05-22 23:22:33 -0500

    Newline after commands

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -87,3 +87,13 @@ function _prompt_left() {
 
 _prompt_right() {
 }
+
+function precmd() {
+  # Print a newline before the prompt, unless it's the
+  # first prompt in the process.
+  if [ -z "$NEW_LINE_BEFORE_PROMPT" ]; then
+    NEW_LINE_BEFORE_PROMPT=1
+  elif [ "$NEW_LINE_BEFORE_PROMPT" -eq 1 ]; then
+    echo ""
+  fi
+}
