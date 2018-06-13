b8e91e844e9ebe1d44dea01019838199d27c7543
Mark Cohen <git@mail.mpc.sh>
2018-06-13 02:14:46 -0500

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Move back to fish!""""""""

---
+++ fish/.config/fish/conf.d/aliases.fish
@@ -0,0 +1,82 @@
+# thefuck
+if type -q thefuck
+  thefuck --alias | source
+end
+
+# allows sudo to use aliases
+function sudo
+  command sudo -sE $argv
+end
+
+# better ls
+function ls
+  command ls -hF --color=auto $argv
+end
+
+function la
+  ls -A $argv
+end
+
+# because I like pain
+function yay
+  command yay --noconfirm $argv
+end
+
+# symlink by default
+function ln
+  command ln -s $argv
+end
+
+# work on directories
+function cp
+  command cp -r $argv
+end
+
+function rm
+  command rm -rf $argv
+end
+
+function mkdir
+  command mkdir -p $argv
+end
+
+# typed racket
+function racket
+  command racket -I typed/racket $argv
+end
+
+# nvim
+function vim
+  command nvim $argv
+end
+
+# rsync options
+function rsync
+  command rsync -avzHE --progress -h $argv
+end
+
+# make SML repl work with arrow keys
+function sml
+  rlwrap sml -Cprint.depth=10000
+end
+
+# mkdir and cd into it
+function mkcd
+  command mkdir -p $argv
+  cd $argv
+end
+
+# cd .. repeatedly
+function up
+  cd (eval printf ../%.0s (seq 1 $argv))
+end
+
+# make CLI rg match fzf
+function rg
+  command rg --hidden --smart-case --glob !.git $argv
+end
+
+# ergodox firmware
+function ergodox
+  command teensy-loader-cli -mmcu=atmega32u4 -v -w $argv
+end
