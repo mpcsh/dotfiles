27a8faf3df055cc2946e650a4fbb5681785fc499
Mark Cohen <m@mpc.sh>
2018-07-17 13:00:59 -0500

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Move back to fish!""""""""""""""
    This reverts commit 08c9c13650fe6653e95cb51b9c196e3f9be867b6.

---
+++ fish/.config/fish/conf.d/aliases.fish
@@ -0,0 +1,102 @@
+# thefuck
+if type -q thefuck
+  thefuck --alias | source
+end
+
+# allows sudo to use aliases
+function sudo
+  command sudo -sE $argv
+end
+
+# better ls
+if type -q exa
+  function ls
+    command exa -1 $argv
+  end
+  function la
+    command exa -1a $argv
+  end
+  function ll
+    command exa -l $argv
+  end
+  function lla
+    command exa -la $argv
+  end
+else
+  function ls
+    command ls -1hF --color=auto $argv
+  end
+  function la
+    command ls -1hFA --color=auto $argv
+  end
+  function ll
+    command ls -lhF --color=auto $argv
+  end
+  function lla
+    command ls -lhFA --color=auto $argv
+  end
+end
+
+# because I like pain
+function yolo
+  command yay --noconfirm $argv
+end
+
+# symlink by default
+function ln
+  command ln -s $argv
+end
+
+# work on directories
+function cp
+  command cp -r $argv
+end
+
+function rm
+  command rm -rf $argv
+end
+
+function mkdir
+  command mkdir -p $argv
+end
+
+# typed racket
+function racket
+  command racket -I typed/racket $argv
+end
+
+# nvim
+function vim
+  command nvim $argv
+end
+
+# rsync options
+function rsync
+  command rsync -avzHE --progress -h $argv
+end
+
+# make SML repl work with arrow keys
+function sml
+  rlwrap sml -Cprint.depth=10000
+end
+
+# mkdir and cd into it
+function mkcd
+  mkdir -p $argv
+  cd $argv
+end
+
+# cd .. repeatedly
+function up
+  cd (eval printf ../%.0s (seq 1 $argv))
+end
+
+# make CLI rg match fzf
+function rg
+  command rg --hidden --smart-case --glob !.git $argv
+end
+
+# ergodox firmware
+function ergodox
+  command teensy-loader-cli -mmcu=atmega32u4 -v -w $argv
+end
