370202081d5f55642fb6ee29a871e50b85705df9
Mark Cohen <m@mpc.sh>
2018-07-19 15:08:09 -0500

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Move back to fish!"""""""""""""""
    This reverts commit d1bff02a58320bddd3d2a0f705ec238b5c4dad33.

---
+++ zsh/.zsh/aliases.zsh
@@ -0,0 +1,93 @@
+# clear default aliases
+unalias -m '*'
+
+###########
+# coreutils
+###########
+
+# allows sudo to use aliases
+alias sudo="sudo -sE "
+
+# better ls
+if type exa &> /dev/null; then
+  alias ls="exa -1"
+  alias la="exa -1a"
+  alias ll="exa -l"
+  alias lla="exa -la"
+else
+  alias ls="ls -1hF --color=auto"
+  alias la="ls -1hFA --color=auto"
+  alias ll="ls -lhF --color=auto"
+  alias lla="ls -lhFA --color=auto"
+fi
+
+# work on directories
+alias cp="cp -r"
+alias rm="rm -rf"
+alias mkdir="mkdir -p"
+
+# symlink by default
+alias ln="ln -s"
+
+# rsync options
+if type rsync &> /dev/null; then
+  alias rsync="rsync -aAXvzHE --progress -h"
+fi
+
+######################
+# default replacements
+######################
+# make CLI rg match fzf
+if type rg &> /dev/null; then
+  alias rg="rg --hidden --smart-case --glob !.git"
+fi
+
+# nvim
+if type nvim &> /dev/null; then
+  alias vim="nvim"
+fi
+
+# typed racket
+if type racket &> /dev/null; then
+  alias racket="racket -I typed/racket"
+fi
+
+# make SML REPL understand up-arrow
+if type rlwrap &> /dev/null && type sml &> /dev/null; then
+  alias sml="rlwrap sml -Cprint.depth=1000"
+fi
+
+# don't ask to edit PKGBUILDs, import PGP keys, etc
+if type yay &> /dev/null; then
+  alias yolo="yay --noconfirm"
+fi
+
+# xclip into clipboard by default
+if type xclip &> /dev/null; then
+  alias xclip="xclip -selection clipboard"
+fi
+
+#########################
+# custom functions & misc
+#########################
+
+# mkdir and cd into it
+function mkcd() {
+  mkdir -p $argv
+  cd $argv
+}
+
+# cd .. repeatedly
+function up() {
+  cd $(eval printf ../%.0s $(seq 1 $argv))
+}
+
+# thefuck
+if type thefuck &> /dev/null; then
+  eval $(thefuck --alias)
+fi
+
+# flash ergodox firmware
+if type teensy-loader-cli &> /dev/null; then
+  alias ergodox="teensy-loader-cli -mmcu=atmega32u4 -w -v"
+fi
---
+++ zsh/.zshrc
@@ -0,0 +1,24 @@
+# load antigen
+source ~/.zsh/antigen/antigen.zsh
+
+# autojump
+antigen bundle rupa/z
+
+# fish-like command suggestions
+antigen bundle zsh-users/zsh-autosuggestions
+
+# syntax highlighting must come last
+antigen bundle zsh-users/zsh-syntax-highlighting
+
+antigen apply
+
+# source config files
+for f in $(ls ~/.zsh/*.zsh); do
+  source $f
+done
+
+# start up tmux if we're not in it
+if type tmuxp &> /dev/null && [ -z $TMUX ]; then
+  export GIT_DUET_CO_AUTHORED_BY=1
+  tmuxp load -y ~/.tmux/sessions/$('ls' -1 ~/.tmux/sessions | sed 's/\.yaml//' | fzf -1 --reverse).yaml
+fi
