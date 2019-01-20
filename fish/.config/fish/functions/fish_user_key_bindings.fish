7906ae0901a8092d1260f00306ceece8e9aa95b0
Mark Cohen <m@mpc.sh>
2019-01-20 17:30:02 -0600

    fish: make git + fzf keybinds properly insert into the current command line

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -5,37 +5,57 @@ end
 
 function __git_fzf_git_status
   __git_fzf_is_in_git_repo; or return
-  git -c color.status=always status --short | \
+  set result (git -c color.status=always status --short | \
     fzf -m --ansi --preview 'git diff --color=always HEAD -- {-1} | head -500' | \
     cut -c4- | \
-    sed 's/.* -> //'
+    sed 's/.* -> //')
+
+  if [ -n "$result" ]
+    commandline -t -- (string escape $result)
+  end
+
   commandline -f repaint
 end
 
 function __git_fzf_git_branch
   __git_fzf_is_in_git_repo; or return
-  git branch -a --color=always | \
+  set result (git branch -a --color=always | \
     grep -v '/HEAD\s' | \
     fzf -m --ansi --preview-window right:70% --preview 'git log --color=always --oneline --graph --date=short \
       --pretty="format:%C(auto)%cd %h%d %s %C(magenta)[%an]%Creset" \
       (echo {} | sed s/^..// | cut -d" " -f1) | head -'$LINES | \
     sed 's/^..//' | cut -d' ' -f1 | \
-    sed 's#^remotes/##'
+    sed 's#^remotes/##')
+
+  if [ -n "$result" ]
+    commandline -t -- (string escape $result)
+  end
+
   commandline -f repaint
 end
 
 function __git_fzf_git_tag
   __git_fzf_is_in_git_repo; or return
-  git tag --sort -version:refname | \
-    fzf -m --ansi --preview-window right:70% --preview 'git show --color=always {} | head -'$LINES
+  set result (git tag --sort -version:refname | \
+    fzf -m --ansi --preview-window right:70% --preview 'git show --color=always {} | head -'$LINES)
+
+  if [ -n "$result" ]
+    commandline -t -- (string escape $result)
+  end
+
   commandline -f repaint
 end
 
 function __git_fzf_git_log
   __git_fzf_is_in_git_repo; or return
-  git log --color=always --graph --date=short --format="%C(auto)%cd %h%d %s %C(magenta)[%an]%Creset" | \
+  set result (git log --color=always --graph --date=short --format="%C(auto)%cd %h%d %s %C(magenta)[%an]%Creset" | \
     fzf -m --ansi --reverse --preview 'git show --color=always (echo {} | grep -o "[a-f0-9]\{7,\}") | head -'$LINES | \
-    sed -E 's/.*([a-f0-9]{7,}).*/\1/'
+    cut -d ' ' -f 3)
+
+  if [ -n "$result" ]
+    commandline -t -- (string escape $result)
+  end
+
   commandline -f repaint
 end
 
