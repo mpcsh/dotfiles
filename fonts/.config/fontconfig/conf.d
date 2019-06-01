ec96af2cdc83fb6fa1f8a282f8a8cd5c7b75eadf
Mark Cohen <m@mpc.sh>
2019-06-01 12:10:37 -0500

    initial move back to arch!

---
+++ fonts/.config/fontconfig/conf.d
@@ -0,0 +1,24 @@
+<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+<fontconfig>
+  <alias>
+    <family>serif</family>
+    <prefer>
+      <family>Noto Serif</family>
+    </prefer>
+  </alias>
+
+  <alias>
+    <family>sans-serif</family>
+    <prefer>
+      <family>Noto Sans</family>
+    </prefer>
+  </alias>
+
+  <alias>
+    <family>monospace</family>
+    <prefer>
+      <family>Iosevka Term Custom</family>
+    </prefer>
+  </alias>
+</fontconfig>
