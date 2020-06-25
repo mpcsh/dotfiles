1291c45b5429e1d888ab515de3b1760b26135104
Mark Cohen <m@mpc.sh>
2020-06-25 12:51:30 -0500

    iterm2: fake pynvim msgpack rpc calls to avoid need for dependencies in auto_light_dark script

--- iterm2/auto_light_dark.py
+++ iterm2/auto_light_dark.py
@@ -2,12 +2,14 @@
 
 import iterm2
 import os
-import pynvim
 import re
-import threading
+import socket
 
 LIGHT_THEME_NAME = "Solarized Light"
 DARK_THEME_NAME = "Solarized Dark"
+
+FAKE_NVIM_ATTACH = b'\x93\x02\xc4\x14nvim_set_client_info\x95\xaepython3-client\x80\xa6remote\x80\x80'
+FAKE_NVIM_SET_COLORSCHEME = b'\x94\x00\x02\xacnvim_command\x91\xb6:call SetColorscheme()'
 
 async def select_preset(connection, theme):
     theme_name = DARK_THEME_NAME if "dark" in theme else LIGHT_THEME_NAME
@@ -21,17 +23,14 @@ async def set_profiles(connection, preset):
         await profile.async_set_color_preset(preset)
 
 
-def set_nvim_helper(cookie):
-    os.environ["ITERM2_COOKIE"] = cookie
-    for socket_id in filter(lambda f: re.match("^nvimsocket-[0-9]+$", f) is not None, os.listdir("/tmp")):
-        socket = "/tmp/" + socket_id
-        nvim = pynvim.attach('socket', path=socket)
-        nvim.command(':call SetColorscheme()')
-
 def set_nvim():
-    t = threading.Thread(target=set_nvim_helper, args=(os.getenv("ITERM2_COOKIE"),))
-    t.start()
-    t.join()
+    for base in filter(lambda f: re.match("^nvimsocket-[0-9]+$", f) is not None, os.listdir("/tmp")):
+        path = "/tmp/" + base
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        sock.connect(path)
+        sock.sendall(FAKE_NVIM_ATTACH)
+        sock.sendall(FAKE_NVIM_SET_COLORSCHEME)
+        sock.close()
 
 
 async def main(connection):
