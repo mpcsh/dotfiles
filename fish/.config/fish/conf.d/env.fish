28b1103bd4b16fd9b8f776a872165f905abd5ad1
Mark Cohen <m@mpc.sh>
2020-07-15 00:22:31 -0500

    meta, iterm2, fish, nvim: switch back to nord with no light mode

--- fish/.config/fish/conf.d/env.fish
+++ fish/.config/fish/conf.d/env.fish
@@ -18,51 +18,25 @@ set -x LESS_TERMCAP_so (printf "\033[01;44;33m")
 set -x LESS_TERMCAP_ue (printf "\033[0m")
 set -x LESS_TERMCAP_us (printf "\033[01;32m")
 
-# mode-dependent colors
-function set_colors
-  set -U fish_color_cancel "red"
-  set -U fish_color_command "foreground" "--bold"
-  set -U fish_color_end "yellow"
-  set -U fish_color_error "red" "--underline"
-  set -U fish_color_escape "cyan"
-  set -U fish_color_normal "normal"
-  set -U fish_color_operator "blue"
-  set -U fish_color_param "normal"
-  set -U fish_color_quote "green"
-  set -U fish_color_redirection "yellow"
-  set -U fish_color_search_match "--reverse"
-  set -U fish_pager_color_completion "normal" "--bold" "--underline"
-  set -U fish_pager_color_description "yellow"
-  set -U fish_pager_color_progress "yellow"
-end
-
-function set_dark_colors
-  set -U fish_pager_color_prefix "brgreen"
-  set -U fish_color_comment "brgreen"
-  set -U fish_color_autosuggestion "brgreen" "--underline"
-  set -Ux FZF_DEFAULT_OPTS "--color=16,fg+:4,bg+:0 --ansi"
-  set_colors
-end
-
-function set_light_colors
-  set -U fish_pager_color_prefix "brcyan"
-  set -U fish_color_comment "brcyan"
-  set -U fish_color_autosuggestion "brcyan" "--underline"
-  set -Ux FZF_DEFAULT_OPTS "--color=16,fg+:4,bg+:7 --ansi"
-  set_colors
-end
-
-if type -q osascript
-  if test (osascript -e 'tell application "System Events" to tell appearance preferences to get dark mode') = "true"
-    set_dark_colors
-  else
-    set_light_colors
-  end
-  trap "set_dark_colors" SIGUSR1
-  trap "set_light_colors" SIGUSR2
-else
-  set_dark_colors
-end
+# colors
+set -U fish_color_autosuggestion "brblack" "--underline"
+set -U fish_color_cancel "red"
+set -U fish_color_command "normal" "--bold"
+set -U fish_color_comment "brblack"
+set -U fish_color_end "yellow"
+set -U fish_color_error "red" "--underline"
+set -U fish_color_escape "cyan"
+set -U fish_color_normal "normal"
+set -U fish_color_operator "blue"
+set -U fish_color_param "normal"
+set -U fish_color_quote "green"
+set -U fish_color_redirection "yellow"
+set -U fish_color_search_match "--reverse"
+set -U fish_pager_color_completion "normal" "--bold" "--underline"
+set -U fish_pager_color_description "yellow"
+set -U fish_pager_color_prefix "brblack"
+set -U fish_pager_color_progress "yellow"
+set -Ux FZF_DEFAULT_OPTS "--color=16,fg+:4 --ansi"
 
 if ! status --is-interactive
   exit
