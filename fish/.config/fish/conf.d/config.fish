801d205d9907bc9472bc24589ba9a93cc30df37f
Mark Cohen <m@mpc.sh>
2021-03-24 10:29:48 -0700

    fish: stop using fish_indent

--- fish/.config/fish/conf.d/config.fish
+++ fish/.config/fish/conf.d/config.fish
@@ -41,45 +41,45 @@ set -U fish_pager_color_progress yellow
 set -Ux FZF_DEFAULT_OPTS "--color=16,fg+:4 --ansi"
 
 function is_apple_silicon
-    test (uname -sm) = "Darwin arm64"
+	test (uname -sm) = "Darwin arm64"
 end
 
 function is_apple_intel
-    test (uname -sm) = "Darwin x86_64"
+	test (uname -sm) = "Darwin x86_64"
 end
 
 # homebrew path
 if is_apple_silicon
-    set -x fish_user_paths /opt/homebrew/bin
+	set -x fish_user_paths /opt/homebrew/bin
 else if is_apple_intel
-    set -x fish_user_paths /usr/local/bin
+	set -x fish_user_paths /usr/local/bin
 end
 
 if ! status --is-interactive
-    exit
+	exit
 end
 
 # prompt
 starship init fish | source
 
 function test_source
-    if test -e $argv[1]
-        source $argv[1]
-    end
+	if test -e $argv[1]
+		source $argv[1]
+	end
 end
 
 function test_exec
-    if type -q $argv[1]
-        $argv[1]
-    end
+	if type -q $argv[1]
+		$argv[1]
+	end
 end
 
 if is_apple_silicon
-    test_source /opt/homebrew/opt/asdf/asdf.fish
-    test_source /opt/homebrew/opt/fzf/shell/key-bindings.fish
+	test_source /opt/homebrew/opt/asdf/asdf.fish
+	test_source /opt/homebrew/opt/fzf/shell/key-bindings.fish
 else if is_apple_intel
-    test_source /usr/local/opt/asdf/asdf.fish
-    test_source /usr/local/opt/fzf/shell/key-bindings.fish
+	test_source /usr/local/opt/asdf/asdf.fish
+	test_source /usr/local/opt/fzf/shell/key-bindings.fish
 end
 
 test_source ~/.asdf/asdf.fish
@@ -92,64 +92,64 @@ test_exec fzf_key_bindings
 
 # allows sudo to use aliases
 function sudo
-    command sudo -sE $argv
+	command sudo -sE $argv
 end
 
 # better ls
 if type -q exa
-    function ls
-        command exa -1 $argv
-    end
-    function la
-        command exa -1a $argv
-    end
-    function ll
-        command exa -lg $argv
-    end
-    function lla
-        command exa -lga $argv
-    end
+	function ls
+		command exa -1 $argv
+	end
+	function la
+		command exa -1a $argv
+	end
+	function ll
+		command exa -lg $argv
+	end
+	function lla
+		command exa -lga $argv
+	end
 end
 
 # work on directories
 function cp
-    command cp -r $argv
+	command cp -r $argv
 end
 
 function rm
-    command rm -rf $argv
+	command rm -rf $argv
 end
 
 function mkdir
-    command mkdir -p $argv
+	command mkdir -p $argv
 end
 
 function rsync
-    command rsync -aAXvzHE $argv
+	command rsync -aAXvzHE $argv
 end
 
 # make CLI rg match fzf
 function rg
-    command rg --hidden --smart-case --glob !.git $argv
+	command rg --hidden --smart-case --glob !.git $argv
 end
 
 # nvim
 function vim
-    command nvim $argv
+	command nvim $argv
 end
 
 # git
 function g
-    command git $argv
+	command git $argv
 end
 
 # mkdir and cd into it
 function mkcd
-    mkdir -p $argv
-    cd $argv
+	mkdir -p $argv
+	cd $argv
 end
 
 # cd .. repeatedly
 function up
-    cd (eval printf ../%.0s (seq 1 $argv))
+	cd (eval printf ../%.0s (seq 1 $argv))
 end
