9f2a23cd2553d10939f700c29f46fb41052a692b
Mark Cohen <git@mail.mpc.sh>
2024-10-17 11:59:29 -0700

    nvim: ensure at least lua, vim, and vimdoc syntax highlighting is installed

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -226,6 +226,8 @@ require("nvim-treesitter.configs").setup({
 		-- Using this option may slow down your editor, and you may see some duplicate highlights.
 		-- Instead of true it can also be a list of languages
 		additional_vim_regex_highlighting = false,
+		ensure_installed = { "lua", "vim", "vimdoc" },
+		auto_install = true,
 	},
 })
 
