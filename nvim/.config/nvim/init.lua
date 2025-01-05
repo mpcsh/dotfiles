d6a8f6d3eca1da10d05f8aa40c3129b3bae666f0
Mark Cohen <git@mail.mpc.sh>
2025-01-05 11:47:04 -0800

    nvim: use prettier and eslint for JSX, use prettier for JSON

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -243,10 +243,12 @@ require("formatter").setup({
 	log_level = vim.log.levels.WARN,
 	filetype = {
 		javascript = { require("formatter.filetypes.javascript").prettier },
+		javascriptreact = { require("formatter.filetypes.javascriptreact").prettier },
 		typescript = { require("formatter.filetypes.typescript").prettier },
 		typescriptreact = { require("formatter.filetypes.typescriptreact").prettier },
 		html = { require("formatter.filetypes.html").prettier },
 		css = { require("formatter.filetypes.css").prettier },
+		json = { require("formatter.filetypes.json").prettier },
 		md = { require("formatter.filetypes.markdown").prettier },
 
 		lua = { require("formatter.filetypes.lua").stylua },
@@ -271,6 +273,7 @@ vim.opt.completeopt = "menu,menuone,noselect"
 
 require("lint").linters_by_ft = {
 	javascript = { "eslint_d" },
+	javascriptreact = { "eslint_d" },
 	typescript = { "eslint_d" },
 	typescriptreact = { "eslint_d" },
 	html = { "eslint_d" },
