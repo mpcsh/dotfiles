5234c5115217d7112a8bdc238f5512b279bec048
Mark Cohen <git@mail.mpc.sh>
2025-05-16 11:08:31 -0700

    nvim: account for breaking changes in mason-lspconfig@2
    https://github.com/mason-org/mason-lspconfig.nvim/releases/tag/v2.0.0

---
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -0,0 +1,15 @@
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				globals = {
+					"vim",
+					"require",
+				},
+			},
+		},
+	},
+})
--- nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -2,24 +2,17 @@ return {
 	"williamboman/mason-lspconfig.nvim",
 	event = { "BufNewFile", "BufReadPost" },
 	dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
-	config = function()
-		require("mason-lspconfig").setup()
-		local lspconfig = require("lspconfig")
-		require("mason-lspconfig").setup_handlers({
-			function(server_name)
-				lspconfig[server_name].setup({})
-			end,
-			["lua_ls"] = function()
-				lspconfig.lua_ls.setup({
-					settings = {
-						Lua = {
-							diagnostics = {
-								globals = { "vim" },
-							},
-						},
-					},
-				})
-			end,
-		})
-	end,
+	config = {
+		ensure_installed = {
+			"basedpyright",
+			"bashls",
+			"buf_ls",
+			"clangd",
+			"cssls",
+			"css_variables",
+			"html",
+			"lua_ls",
+			"ts_ls",
+		},
+	},
 }
