cd7c819698a57403cc9805ce366dff4ab5fd54c0
Mark Cohen <git@mail.mpc.sh>
2025-05-18 03:05:34 -0700

    nvim: show hidden files in oil, don't hide oil buffer

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -1,8 +1,15 @@
 return {
 	"stevearc/oil.nvim",
-	opts = {},
 	-- dependencies = { { "echasnovski/mini.icons", opts = {} } },
 	dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
 	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
 	lazy = false,
+	opts = {
+		buf_options = {
+			bufhidden = "",
+			buflisted = true,
+		},
+		natural_order = false,
+		show_hidden = true,
+	},
 }
