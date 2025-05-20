251efd73a735a61e4438ac74b68436b18106fd58
Mark Cohen <git@mail.mpc.sh>
2025-05-19 17:49:24 -0700

    nvim: turns out there's already a maintained version of mason-conform

--- dot_config/nvim/lua/plugins/mason-conform.lua
+++ dot_config/nvim/lua/plugins/mason-conform.lua
@@ -1,5 +1,5 @@
 return {
-	"mpcsh/mason-conform",
+	"zapling/mason-conform.nvim",
 	dependencies = { "williamboman/mason.nvim", "stevearc/conform.nvim" },
 	event = { "BufNewFile", "BufReadPost" },
 	opts = {},
