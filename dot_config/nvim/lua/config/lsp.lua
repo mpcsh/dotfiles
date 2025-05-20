32fb4a74d1f35b2066b70e7787056de03b8e811b
Mark Cohen <git@mail.mpc.sh>
2025-05-20 00:24:02 -0700

    nvim: ignore unused variable warnings from ts_ls (caught by eslint_d)

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -7,3 +7,12 @@ vim.lsp.config("lua_ls", {
 		},
 	},
 })
+
+local ignoredCodes = { 6133, 6196 }
+vim.lsp.config("ts_ls", {
+	settings = {
+		diagnostics = {
+			ignoredCodes = ignoredCodes,
+		},
+	},
+})
