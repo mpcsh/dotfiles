75db805e5f8250b1b6dccd0e5e234f7cc861abea
Mark Cohen <git@mail.mpc.sh>
2026-08-11 05:13:41 -0700

    nvim: mason-conform was less lazy than conform

--- dot_config/nvim/lua/plugins/mason-conform.lua
+++ dot_config/nvim/lua/plugins/mason-conform.lua
@@ -1,6 +1,6 @@
 return {
 	"zapling/mason-conform.nvim",
 	dependencies = { "williamboman/mason.nvim", "stevearc/conform.nvim" },
-	event = { "BufNewFile", "BufReadPost" },
+	event = { "BufWritePre" },
 	opts = {},
 }
