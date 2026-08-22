2eb05782f382aaf7ce128ab39ca783814f770c95
Mark Cohen <git@mail.mpc.sh>
2026-08-22 00:44:57 -0700

    nvim: rotate lsp log at 10MiB

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -1,3 +1,10 @@
+local lsp_log = vim.fn.stdpath("state") .. "/lsp.log"
+local lsp_log_stat = vim.uv.fs_stat(lsp_log)
+if lsp_log_stat and lsp_log_stat.size > 10 * 1024 * 1024 then
+	os.rename(lsp_log, vim.fn.stdpath("state") .. "/" .. os.date("%Y-%m-%dT%H:%M:%S") .. ".lsp.log")
+end
+vim.lsp.log.set_level(vim.log.levels.WARN)
+
 vim.lsp.config("lua_ls", {
 	settings = {
 		Lua = {
