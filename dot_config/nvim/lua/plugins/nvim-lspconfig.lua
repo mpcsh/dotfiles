fe238b853c4d7ed5f8f4bb8910d74280f8260582
Mark Cohen <git@mail.mpc.sh>
2026-09-10 14:44:12 -0700

    nvim: fix dependency cycle between nvim-lspconfig and mason-lspconfig

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,10 +1,4 @@
 return {
 	"neovim/nvim-lspconfig",
 	event = { "BufNewFile", "BufReadPost" },
-	dependencies = {
-		{
-			"mason-org/mason-lspconfig.nvim",
-			dependencies = { "mason-org/mason.nvim" },
-		},
-	},
 }
