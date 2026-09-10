4afe53b4b27aea8e3368f0750a14a9b9fa4232f0
Mark Cohen <git@mail.mpc.sh>
2026-09-10 15:31:03 -0700

    nvim: remove stale comment from pre-chezmoi era

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,7 +1,6 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	event = "UIEnter", -- TODO: this prevents lualine from loading on a bare launch with no file
-	-- TODO: ORRRR apparently lazy.nvim doesn't like symlinks?! As soon as I stowed `nvim` without `--no-folding` everything works?!
+	event = "UIEnter",
 	opts = {
 		options = {
 			component_separators = { left = "▓▒░", right = "░▒▓" },
