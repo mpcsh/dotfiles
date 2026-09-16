363debac90cf2361d57a6df8ca9f5fc426ab2e90
Mark Cohen <git@mail.mpc.sh>
2026-09-16 15:11:26 -0700

    nvim: allow selecting & closing the current buffer in fzf's buffers picker

--- dot_config/nvim/lua/plugins/fzf-lua.lua
+++ dot_config/nvim/lua/plugins/fzf-lua.lua
@@ -2,6 +2,36 @@ return {
 	"ibhagwan/fzf-lua",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {
+		buffers = {
+			actions = {
+				-- this allows closing the current buffer
+				["ctrl-x"] = {
+					fn = function(selected, opts)
+						local path = require("fzf-lua.path")
+						local snacks = require("snacks")
+						for _, sel in ipairs(selected) do
+							local entry = path.entry_to_file(sel, opts)
+							if entry.bufnr then
+								snacks.bufdelete({ buf = entry.bufnr, wipe = true })
+							end
+						end
+					end,
+					reload = true,
+				},
+			},
+			fzf_opts = {
+				-- this makes the current buffer selectable
+				["--header-lines"] = false,
+				-- this is required for the below to work
+				["--sync"] = true,
+			},
+			keymap = {
+				fzf = {
+					-- this restores the original functionality of preselecting the next most recent buffer
+					start = "pos(2)",
+				},
+			},
+		},
 		colorschemes = {
 			ignore_patterns = {
 				"^blue$",
