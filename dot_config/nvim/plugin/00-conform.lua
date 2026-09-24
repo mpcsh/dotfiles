d2dbf83e27878ca82ec4cf514acc7e633ba01a1c
Mark Cohen <git@mail.mpc.sh>
2026-09-23 17:50:37 -0700

    nvim: skip format-on-save when editing temporary git buffers, patches, etc

--- dot_config/nvim/plugin/00-conform.lua
+++ dot_config/nvim/plugin/00-conform.lua
@@ -1,4 +1,12 @@
 vim.pack.add({ "https://github.com/stevearc/conform.nvim" })
+
+local skip_format = {
+	diff = true,
+	git = true,
+	gitcommit = true,
+	gitrebase = true,
+}
+
 require("conform").setup({
 	formatters_by_ft = {
 		cpp = { "clang-format" },
@@ -16,5 +24,10 @@ require("conform").setup({
 		yaml = { "yamlfmt" },
 		["_"] = { "trim_whitespace" },
 	},
-	format_on_save = { lsp_format = "fallback" },
+	format_on_save = function(bufnr)
+		if skip_format[vim.bo[bufnr].filetype] then
+			return nil
+		end
+		return { lsp_format = "fallback" }
+	end,
 })
